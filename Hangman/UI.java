351f4af685612e7fab7dcd84d9a0de01c2717313
Moritz <47506586+Moritz-github@users.noreply.github.com>
2020-06-04 18:47:17 +0200

    added random selection from text file

--- Hangman/UI.java
+++ Hangman/UI.java
@@ -1,7 +1,24 @@
 package me.mo;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class UI {
 	public static void main(String[] args) {
-		HangMan hm = new HangMan("Gartenzwerg", 5, true);
+		ArrayList<String> words = new ArrayList<String>();
+		try {
+			File f = new File("C:/Users/Motop/Desktop/MEGAsync/Coding/Java/Köllö/Programs/Eclipse_Workspace/Hangman/src/me/mo/words.txt");
+			Scanner reader = new Scanner(f);
+			while(reader.hasNextLine()) {
+				words.add(reader.nextLine());
+			}
+			reader.close();
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		Random r = new Random();
+		HangMan hm = new HangMan(words.get(r.nextInt(words.size())), 7);
 	}
 }
