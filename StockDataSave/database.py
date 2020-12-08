0b0688fa3221090ba443f354247c041e8e408dbf
Moritz-github <47506586+Moritz-github@users.noreply.github.com>
2020-12-08 11:52:08 +0100

    database integration

---
+++ StockDataSave/database.py
@@ -0,0 +1,40 @@
+# importing mysql-connector-python
+import mysql.connector
+
+aktie = "AAPL"
+
+with open("mysql-pwd.txt") as file:
+    pwd = file.readlines()[0]
+
+
+class DBManager:
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+        self.db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd=pwd
+        )
+        self.cursor = self.db.cursor()
+
+        self.cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(self.symbol))
+        self.cursor.execute("USE {}".format(self.symbol))
+        # self.cursor.execute("DROP TABLE {}".format(self.symbol))
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS {} ( date DATE PRIMARY KEY, price DECIMAL(15,2) )".format(self.symbol))
+
+    def add_value(self, datestring, price):
+        self.cursor.execute("REPLACE INTO {} (date, price) VALUES (DATE(\"{}\"), {})".format(self.symbol, datestring, price))
+        self.db.commit()
+
+    def print_all_values(self):
+        self.cursor.execute("SELECT * FROM {}".format(self.symbol))
+
+        for x in self.cursor:
+            print(x)
+
+
+if __name__ == "__main__":
+    db = DBManager("AAPL")
+    # db.add_value("2020-07-17", 385.3100)
+    db.print_all_values()
--- StockDataSave/main.py
+++ StockDataSave/main.py
@@ -12,6 +12,11 @@ full
 """
 import requests
 import json
+import database
+
+user_symbol = input("Bitte gib eine Aktie an (Apple ... AAPL; Tesla ... TSLA)\n")
+
+db = database.DBManager(user_symbol)
 
 # Read API-Key from Text file
 with open("api-key.txt") as file:
@@ -33,6 +38,18 @@ def request(url):
     return json.loads(requests.get(url).text)
 
 
+def write_close_values_db(symbol):
+    url = URLBuilder(function="TIME_SERIES_DAILY", symbol=symbol)
+    response = request(url)
+    days = response["Time Series (Daily)"]
+
+    for day in days:
+        close = days[day]["4. close"]
+        db.add_value(day, float(close))
+
+        # print("{}: Close: {}".format(day, close))
+
+
 def get_close_values(symbol):
     url = URLBuilder(function="TIME_SERIES_DAILY", symbol=symbol)
     response = request(url)
@@ -42,4 +59,5 @@ def get_close_values(symbol):
         close = days[day]["4. close"]
         print("{}: Close: {}".format(day, close))
 
-print(get_close_values("AAPL"))
+
+print(write_close_values_db(user_symbol))
