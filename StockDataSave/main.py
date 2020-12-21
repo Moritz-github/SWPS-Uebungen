4fb536e76bd9ef4a00b306e8cfdcf1fe9642687a
Moritz-github <47506586+Moritz-github@users.noreply.github.com>
2020-12-21 08:56:28 +0100

    200 avg

--- StockDataSave/main.py
+++ StockDataSave/main.py
@@ -6,10 +6,21 @@ user_symbol = input("Bitte gib eine Aktie an (Apple ... AAPL; Tesla ... TSLA; Am
 db = database.DBManager(user_symbol)
 db.init_table()
 
-count = 0
+prices = []
+
 for daily_prices in scrape.get_daily_prices(user_symbol, outputsize="full"):
     db.add_row_from_daily_price(daily_prices)
-    count += 1
-print(f"Added {count} rows to the {user_symbol} table in the STOCKDATA database")
+
+    prices.append(daily_prices)
 
 db.close()
+
+print(f"Added {len(prices)} rows to the {user_symbol} table in the STOCKDATA database")
+
+endlen = len(prices)-1
+for i in range(endlen, endlen-100, -1):
+    print(prices[i])
+    print("200 Day Avg.: ", end="")
+    twoHundredAVG = sum([x.close for x in prices[i-200:i]]) / 200
+    print(twoHundredAVG)
+    print("\n")
